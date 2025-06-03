6f3dc05f6bcd968867ed08a279f6b519c80d0263
Shaneos44 <shaneosullivan44@gmail.com>
2025-06-03 11:28:30 +0800

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,10 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "YOUR_KEY",
-  authDomain: "YOUR_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  // ...etc...
+  apiKey: "AIzaSyAU2AymyTHBwIpEj7eVw4hwVrBlvHO4zIY",
+  authDomain: "vt-production-planner.firebaseapp.com",
+  projectId: "vt-production-planner",
+  storageBucket: "vt-production-planner.firebasestorage.app",
+  messagingSenderId: "51893208612",
+  appId: "1:51893208612:web:80e747705d13932a23fab1"
 };
 
 export const app = initializeApp(firebaseConfig);
